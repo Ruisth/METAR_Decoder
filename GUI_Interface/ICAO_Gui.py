d77ebe7c532e09ae6b37fa7a94b1d07e883b77fb
Ruisth <49766880+Ruisth@users.noreply.github.com>
2024-02-09 00:26:39 +0000

    Homepage creation
    Signed-off-by: Ruisth <ruimfduarte94@gmail.com>

---
+++ GUI_Interface/ICAO_Gui.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+
+
+def icao():
+    print("Hello World!", )
+
+
+if __name__ == '__icao__':
+    icao()
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from GUI_Interface.ICAO_Gui import icao
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Homepage(App):
+    def build(self):
+        return Button(text="Welcome to LikeGeeks!", background_color=(255, 0, 0, 100))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+Homepage().run()
