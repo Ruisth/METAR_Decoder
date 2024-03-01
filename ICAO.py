617ca6d7404fc1693866839d72c80974f188906e
Ruisth <ruimfduarte94@gmail.com>
2024-03-01 16:51:49 +0000

    Label with METAR Code output implemented
    Signed-off-by: Ruisth <ruimfduarte94@gmail.com>

--- ICAO.py
+++ ICAO.py
@@ -2,12 +2,14 @@ import requests
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
+from kivy.uix.label import Label
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.screenmanager import Screen
 from kivy.uix.textinput import TextInput
 
 
 class ICAOScreen(Screen):
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -20,8 +22,15 @@ class ICAOScreen(Screen):
         # Input Box
         icao_box = BoxLayout(orientation='horizontal', size_hint=(None, None), size=(600, 100))
         icao_box.pos_hint = {'center_x': 0.5, 'center_y': 0.8}
-        self.icao_input = TextInput(hint_text="Enter ICAO code", size_hint=(0.5, 1), background_color=(0.082, 0.322, 0.388, 1))
+        self.icao_input = TextInput(hint_text="Enter ICAO code", size_hint=(0.5, 1),
+                                    background_color=(0.082, 0.322, 0.388, 1))
         icao_box.add_widget(self.icao_input)
+
+        # Response Box
+        response_box = BoxLayout(orientation='horizontal', size_hint=(None, None), size=(600, 100))
+        response_box.pos_hint = {'center_x': 0.5, 'center_y': 0.6}
+        self.response_label = Label(text="")
+        response_box.add_widget(self.response_label)
 
         # Buttons Layout
         buttons_layout = BoxLayout(orientation='horizontal', size_hint=(None, None), size=(600, 100), spacing=50)
@@ -38,6 +47,7 @@ class ICAOScreen(Screen):
         buttons_layout.add_widget(search_button)
         buttons_layout.add_widget(back_button)
         layout.add_widget(icao_box)
+        layout.add_widget(response_box)
         layout.add_widget(buttons_layout)
         self.add_widget(layout)
 
@@ -47,6 +57,7 @@ class ICAOScreen(Screen):
         response = requests.get(url)
         if response.status_code == 200:
             metar_data = response.text
+            self.response_label.text = metar_data
             print("METAR Data: ")
             print(metar_data)
             return response.text
@@ -55,5 +66,7 @@ class ICAOScreen(Screen):
             return None
 
     def home_screen(self, instance):
-        self.icao_input.text = ""
+
         self.manager.current = 'Home Page'
+        self.icao_input.text = ""
+        self.response_label.text = ""
